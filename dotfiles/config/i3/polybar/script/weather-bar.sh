28d9099aecd92ffa8f0c786bb77cdb55123a20e9
wiwyil2tr <108447154+wiwyil2tr@users.noreply.github.com>
2024-04-28 09:57:15 +0800

    Add files via upload

---
+++ dotfiles/config/i3/polybar/script/weather-bar.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#curl -sS "wttr.in/?format=%t"
+#curl -sS "wttr.in/37.148889,115.339167?format="%c+%t+%h""
+#curl -sS "wttr.in/xingtai?format="%c+%t+%h""
+curl -sS "wttr.in/Harbin?format=3"
